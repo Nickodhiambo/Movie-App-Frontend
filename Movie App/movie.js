e7b6d59c6bc9eb8c39bec4b64d1c107185105c6a
Nick <nodhiambo01@gmail.com>
2024-04-12 09:56:02 +0300

    Fix bugs

--- Movie App/movie.js
+++ Movie App/movie.js
@@ -21,38 +21,41 @@ div_new.innerHTML = `
                 <p><strong>User: </strong>
                     <input type='text' id='new_user' value='' />
                 </p>
-                <p><a href='#' onclick=saveReview('new_review', 'new_user')>Save</a></p>
+                <p><a href='#' onclick="saveReview('new_review', 'new_user')">Save</a></p>
+            </div`
 
-            `
 row.appendChild(div_new);
 
 returnReviews(APILINK);
 
 async function returnReviews(url) {
-    response = await (await fetch(url + 'movie/' + movieId)).json();
-    console.log(response);
+    // response = await (await fetch(url + 'movie/' + movieId)).json();
+    fetch(url + 'movie/' + movieId)
+        .then(res => res.json())
+        .then(function (data) {
+            console.log(data);
+            data.forEach(review => {
 
-    response.forEach(review => {
+                //Dynamically create containers for API data
+                const columnDiv = document.createElement('div');
+                columnDiv.setAttribute('class', 'column');
 
-        //Dynamically create containers for API data
-        const columnDiv = document.createElement('div');
-        columnDiv.setAttribute('class', 'column');
+                columnDiv.innerHTML = `
+                    <div class="card" id='${review._id}'>
+                        <p><strong>Review: </strong>${review.review}</p>
+                        <p><strong>User: </strong>${review.user}</p>
+                        <p>
+                            <a href='#' onclick="editReview('${review._id}', '${review.review}', '${review.user}')">Edit</a>
+                            <a href='#' onclick="deleteReview('${review._id}')">Delete</a>
+                        </p>
+                    </div>`
 
-        columnDiv.innerHTML = `
-                <div class="card" id=${review._id}>
-                    <p><strong>Review: </strong>${review.review}</p>
-                    <p><strong>User: </strong>${review.user}</p>
-                    <p>
-                        <a href='#' onclick="editReview('${review._id}', '${review.review}', '${review.user}')">Edit</a>
-                        <a href='#' onClick="deleteReview('${review._id}')">Delete</a>
-                    </p>
-                </div>`
-
-        row.appendChild(columnDiv);
-    });
+                row.appendChild(columnDiv);
+            });
+        });
 }
 
-function editReview(id, review, user){
+function editReview(id, review, user) {
     const element = document.getElementById(id)
     const reviewInputId = 'review' + id;
     const userInputId = 'user' + id;
@@ -64,52 +67,52 @@ function editReview(id, review, user){
                     <p><strong>User: </strong>
                         <input type='text' id="${userInputId}" value="${user}" />
                     </p>
-                    <p><a href="#" onclick=saveReview('${reviewInputId}', '${userInputId}', '${id}')>Save review</a></p>
+                    <p><a href="#" onclick="saveReview('${reviewInputId}', '${userInputId}', '${id}')">Save review</a></p>
                 `
 }
 
-function saveReview(reviewInputId, userInputId, id=""){
+function saveReview(reviewInputId, userInputId, id="") {
     const review = document.getElementById(reviewInputId).value;
     const user = document.getElementById(userInputId).value
 
-    if (id){
+    if (id) {
         fetch(APILINK + id, {
             method: 'PUT',
-            headers:{
+            headers: {
                 'Accept': 'application/json, text/plain, */*',
                 'Content-Type': 'application/json'
             },
-            body: JSON.stringify({'review': review, 'user': user})
+            body: JSON.stringify({ 'review': review, 'user': user })
         })
+            .then(res => res.json())
+            .then(data => {
+                console.log(data);
+                location.reload();
+            });
+    } else {
+        fetch(APILINK + 'new', {
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json, text/plain, */*',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ "user": user, "review": review, "movieId": movieId })
+        })
+            .then(res => res.json())
+            .then(data => {
+                console.log(data);
+                location.reload();
+            });
+    }
+}
+
+function deleteReview(id) {
+    fetch(APILINK + id, {
+        method: 'DELETE'
+    })
         .then(res => res.json())
         .then(data => {
             console.log(data);
             location.reload();
-        });
-    } else {
-        fetch(APILINK + 'new', {
-            method: 'POST',
-            headers:{
-                'Accept': 'application/json, text/plain, */*',
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({"user": user, "review": review, "movieId": movieId})
         })
-          .then(res => res.json())
-          .then(data => {
-            console.log(data);
-            location.reload();
-          });
-    }
-}
-
-function deleteReview(id){
-    fetch(APILINK + id, {
-        method: 'DELETE'
-    })
-      .then(res => res.json())
-      .then(data => {
-        console.log(data);
-        location.reload();
-      })
 }
